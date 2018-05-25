acd2d445503f7b56433427a618246892c0b8f0e7
Anthony Nahas <anthony.na@hotmail.de>
2018-05-25 23:08:21 +0200

    feat(host): added nodejs server to host the pwa with express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path')
+const compression = require('compression');
+
+const app = express();
+
+// compress all responses
+app.use(compression());
+
+app.use(serveStatic(path.join(__dirname, 'dist')));
+app.listen(3000);
